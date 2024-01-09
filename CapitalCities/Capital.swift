aa10bf3fdec538e0ab5d7e6a45e4962f5940b572
alibekshak <alibekshakiro1@gmail.com>
2024-01-09 21:10:25 +0600

    Adding capital cities.

---
+++ CapitalCities/Capital.swift
@@ -0,0 +1,22 @@
+//
+//  Capital.swift
+//  CapitalCities
+//
+//  Created by Apple on 09.01.2024.
+//
+
+import UIKit
+import MapKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String? = nil, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+    
+}
--- CapitalCities/ViewController.swift
+++ CapitalCities/ViewController.swift
@@ -6,12 +6,28 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var mapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let london = Capital(title: "London", coordinate: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), info: "City of money")
+        
+        let oslo = Capital(title: "Oslo", coordinate: CLLocationCoordinate2D(latitude: 59.95, longitude: 10.75), info: "City of vikings")
+        
+        let paris = Capital(title: "Paris", coordinate: CLLocationCoordinate2D(latitude: 48.8567, longitude: 2.3508), info: "City of culutre")
+        
+        let rome = Capital(title: "Rome", coordinate: CLLocationCoordinate2D(latitude: 41.9, longitude: 12.5), info: "City of history")
+        
+        let washington = Capital(title: "Washington DC", coordinate: CLLocationCoordinate2D(latitude: 38.895111, longitude: -77.036667), info: "USA!")
+        
+        let capital = [london, oslo, paris, rome, washington]
+        
+        mapView.addAnnotations(capital)
     }
 
 
